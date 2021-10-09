3f595f861c87e6f41eddd543cfdc8c3a95777247
lb2281075105 <2281075105@qq.com>
2021-10-09 22:41:54 +0800

    初始目录结构

--- template-mobile/src/App.vue
+++ template-mobile/src/App.vue
@@ -1,28 +1,21 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <router-view />
+    <h1>heima</h1>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+/* 全局样式在这里设置也可以 */
+/*@import "styles/index.less";*/
 </style>
--- template-mobile/src/main.js
+++ template-mobile/src/main.js
@@ -1,8 +1,15 @@
 import Vue from 'vue'
 import App from './App.vue'
+import router from "./router/index.js";
+import store from './store/index.js'
+
+// 加载全局样式
+import './styles/index.less'
 
 Vue.config.productionTip = false
 
 new Vue({
+  router,
+  store,
   render: h => h(App),
 }).$mount('#app')
