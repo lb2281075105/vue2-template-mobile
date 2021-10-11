367a485ef6fad42a2a3f5dfcc76b0e5639c1c219
lb2281075105 <2281075105@qq.com>
2021-10-12 06:09:25 +0800

    vant

--- template-mobile/src/App.vue
+++ template-mobile/src/App.vue
@@ -1,7 +1,14 @@
 <template>
   <div id="app">
     <router-view />
+    <van-button type="primary">主要按钮</van-button>
+    <van-button type="info">信息按钮</van-button>
+    <van-button type="default">默认按钮</van-button>
+    <van-button type="warning">警告按钮</van-button>
+    <van-button type="danger">危险按钮</van-button>
     <h1>heima</h1>
+    <i class="toutiao toutiao-dianzan2"></i>
+    <h1>标题</h1>
   </div>
 </template>
 
@@ -18,4 +25,5 @@ export default {
 <style>
 /* 全局样式在这里设置也可以 */
 /*@import "styles/index.less";*/
+
 </style>
--- template-mobile/src/main.js
+++ template-mobile/src/main.js
@@ -1,10 +1,22 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from "./router/index.js";
-import store from './store/index.js'
-
+// 如果里面store里面有index.js可以不写index.js 就会自动找index.js
+import store from './store'
 // 加载全局样式
 import './styles/index.less'
+
+//加载动态设置REM基准值:就是行内样式转换不了
+import 'amfe-flexible'
+
+// 加载vant核心组件库
+import Vant from 'vant'
+// 加载vant全局样式
+import 'vant/lib/index.css'
+// 注册使用vant组件库
+Vue.use(Vant)
+
+
 
 Vue.config.productionTip = false
 
